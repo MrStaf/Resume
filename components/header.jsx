034acc573e16f7e3d54634ef530ebc88ab697e3f
mrstaf <ben82160@gmail.com>
2021-07-13 01:32:27 +0200

    Link update and incorrect meta tag deleted

--- components/header.jsx
+++ components/header.jsx
@@ -5,14 +5,11 @@ export default function Header() {
   return (
     <>
       <Head>
-        <html lang="en-US" />
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Benoît Fage is " />
-        <link rel="shortcut icon" href="favicon.svg" type="image/x-icon" />
+        <meta name="description" content="Benoît Fage resume" />
+        <link rel="icon" href="favicon.ico" />
       </Head>
       <header className="flex flex-row justify-between px-4 pt-4 text-2xl select-none print:hidden sm:px-16 sm:pt-16">
-        <Link href="/">
+        <Link href="/" prefetch={false}>
           <div
             className="p-2 pt-0 text-center border-b-2 cursor-pointer"
             style={{ width: "200px" }}
@@ -20,7 +17,7 @@ export default function Header() {
             Resume
           </div>
         </Link>
-        <Link href="/projects">
+        <Link href="/projects" prefetch={false}>
           <div
             className="p-2 pt-0 text-center border-b-2 cursor-pointer"
             style={{ width: "200px" }}
